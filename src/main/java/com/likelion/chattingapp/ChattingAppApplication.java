d38214dbcb879095267c7b4298bc2fa9f2f37534
yejinstar <gyj5586@gmail.com>
2022-12-19 10:17:06 +0900

    first commit

---
+++ src/main/java/com/likelion/chattingapp/ChattingAppApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.chattingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/chattingapp/ChattingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.chattingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
